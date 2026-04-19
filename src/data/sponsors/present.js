fe7573762cd43afbd23ac96816179ed0c2bb1787
Anurag019-hub <anurag160507@gmail.com>
2026-04-19 16:16:17 +0530

    commit

--- src/data/sponsors/present.js
+++ src/data/sponsors/present.js
@@ -2,7 +2,7 @@ export const present = [
     {
         "name": "NVCTI",
         "tier": "Title",
-        "status": "present",
+        "status": "current",
         "website": "https://www.nvcti.in/",
         "logo": "/images/sponsors/nvcti.webp",
         "description": "The centre aims to provide a supportive environment where students can transform their ideas into real-world products and innovative solutions."
